353136d4ff589e6738c3afa26257edf2f943a1f2
ilemuelfigueira <lfigueiradev@gmail.com>
2023-01-09 03:22:25 -0300

    feat: tipagem do process.env

---
+++ src/@types/global.d.ts
@@ -0,0 +1,10 @@
+
+// node env typings
+declare namespace NodeJS {
+  interface ProcessEnv {
+    APP_LINKEDIN_COOKIE: string;
+    APP_LINKEDIN_URL: string = "https://www.linkedin.com";
+    APP_LINKEDIN_PROFILE_URL: string;
+    APP_LINKEDIN_PROFILE_EXPERIENCE_PATH: string = "/detail/experience";
+  }
+}
