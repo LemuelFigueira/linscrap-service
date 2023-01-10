ec5c777e811b4e65ffd56baf34abddab08cbad1e
ilemuelfigueira <lfigueiradev@gmail.com>
2023-01-10 17:28:58 -0300

    feat: log service
    instalando dependencia moment para buscar data atual

---
+++ src/services/LogService.ts
@@ -0,0 +1,34 @@
+import moment from "moment";
+
+export class LogService {
+
+  private name: string
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  info(...args: any[]) {
+
+    console.info(`\x1b[1;36m${this.now} - ${this.name}\x1b[0m`, ...args)
+  }
+
+  error(...args: any[]) {
+
+    console.info(`\x1b[1;31m${this.now} - ${this.name}\x1b[0m`, ...args)
+  }
+
+  debug(...args: any[]) {
+
+    console.info(`\x1b[1;33m${this.now} - ${this.name}\x1b[0m`, ...args)
+  }
+  
+  log(...args: any[]) {
+
+    console.info(`\x1b[1;37m${this.now} - ${this.name}\x1b[0m`, ...args)
+  }
+
+  private get now() {
+    return moment().utc().format('YYYY-MM-DD HH:mm:ss')
+  }
+}
