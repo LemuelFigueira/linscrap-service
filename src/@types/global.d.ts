96f66cf6f8a35bfac922f0e8388f31ade1159406
ilemuelfigueira <lfigueiradev@gmail.com>
2023-01-10 20:58:37 -0300

    feat: atualizando tipagem process env e .env-example

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -4,7 +4,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     APP_LINKEDIN_COOKIE: string;
     APP_LINKEDIN_URL: string = "https://www.linkedin.com";
-    APP_LINKEDIN_PROFILE_URL: string;
     APP_LINKEDIN_PROFILE_EXPERIENCE_PATH: string = "/detail/experience";
   }
 }
