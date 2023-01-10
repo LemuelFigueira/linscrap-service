cfd00b7361cf47269212a0467347db0e03cfc0cb
ilemuelfigueira <lfigueiradev@gmail.com>
2023-01-10 19:14:43 -0300

    feat: Apiconstants

---
+++ src/utils/ApiConstants.ts
@@ -0,0 +1,11 @@
+export class ApiConstants {
+
+  static PROFILE_ROUTE = "/profiles"
+
+  static STARTING_BROWSER = "Starting browser"
+  static OPENING_NEW_PAGE = "Opening new page"
+
+  static RETRIEVING_PERFIL_HEADER = "Retrieving perfil header"
+  static RETRIEVING_PERFIL_EXPERIENCES = "Retrieving perfil experiences"
+  
+}
