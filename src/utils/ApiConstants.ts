60f4375e6fd0becd3acb031cae0eaef3bc54b302
ilemuelfigueira <lfigueiradev@gmail.com>
2023-01-10 20:54:34 -0300

    feat: adicionando api constants util para controle de mensagens

--- src/utils/ApiConstants.ts
+++ src/utils/ApiConstants.ts
@@ -8,4 +8,13 @@ export class ApiConstants {
   static RETRIEVING_PERFIL_HEADER = "Retrieving perfil header"
   static RETRIEVING_PERFIL_EXPERIENCES = "Retrieving perfil experiences"
   
+  static ERROR_INVALID_PARAMETERS = "Invalid parameters provided"
+  static ERROR_REQUIRED_PARAMETER = "Required parameter $s not provided"
+  static ERROR_REQUIRED_QUERY_PARAMETER = "Required query parameter $s not provided"
+  static ERROR_REQUIRED_HEADER = "Required header $s not provided"
+
+  static formatar(param: keyof Omit<typeof ApiConstants, 'prototype'>, ...args: any[]){
+    return `${ApiConstants[param]}`.replace(/\$s/g, args[0])
+  }
+  
 }
