54585a93292d0f3266163885ce39ab7639b6217a
ilemuelfigueira <lfigueiradev@gmail.com>
2023-01-10 21:03:24 -0300

    feat: adicionando expressjson e gitignore .vercel

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,12 @@ import { profileRouter } from './routes/ProfileRoute';
 
 const app = express();
 
+app.use(express.json())
+
+app.get("*", (req, res) => {
+  res.status(404).send("Resource Not Found");
+});
+
 app.use('/profiles',profileRouter)
 
 export {
