8741bd8ff92378e0c38b3d4e5b865a0df0d31958
ilemuelfigueira <lfigueiradev@gmail.com>
2023-01-10 21:23:24 -0300

    fix: execucao function vercel
    app.get(*) estava antes e isso encarretava em sobrescrever as rotas get

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import { app } from "./server";
 import { LogService } from "@services/LogService";
+import { profileRouter } from '@routes/ProfileRoute';
+import express, { urlencoded } from 'express';
 
 require('dotenv').config()
 
+const app = express();
 const port = process.env.PORT || 3000;
 
 const log = new LogService('SERVER')
+
+app.use(express.json())
+
+app.use('/profiles', profileRouter)
+
+app.get("*", (req, res) => {
+  res.status(404).send("Resource Not Found");
+});
+
+
+app.use(urlencoded({ extended: true, limit: "50mb" }))
 
 app.listen(port, () => {
   log.info(`Server running on port ${port}`)
